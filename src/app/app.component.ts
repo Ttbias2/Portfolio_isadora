a04a7d796ab9d592c236cd604ee66775a1ab2991
Ttbias2 <tobiasvalentini73@gmail.com>
2024-05-15 15:23:42 -0300

    portfolio v1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PresentacionComponent } from './presentacion/presentacion.component';
+import { GaleriaComponent } from './galeria/galeria.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,PresentacionComponent,GaleriaComponent,ContactoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Portfolio_Isadora';
+  mostrar = 1;
 }
